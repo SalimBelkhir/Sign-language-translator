ca00a9ed1ac9b7d4f6299e34fce2fe114c863f47
Salim Belkhir <143688571+SalimBelkhir@users.noreply.github.com>
2024-06-03 06:38:11 +0100

    Update FeatureExtraction.py

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -4,46 +4,51 @@ import mediapipe as mp
 import cv2
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=True ,min_detection_confidence=0.3)
-DATA_DIR='./data'
-data=[]
-labels=[]
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+DATA_DIR = './data'
+data = []
+labels = []
 
 for dir_ in os.listdir(DATA_DIR):
-    class_dir = os.path.join(DATA_DIR,dir_)
+    class_dir = os.path.join(DATA_DIR, dir_)
     if not os.path.isdir(class_dir):
         continue
     for img_path in os.listdir(class_dir):
-        data_aux=[]
+        data_aux = []
         x_ = []
         y_ = []
-        img_file = os.path.join(class_dir,img_path)
+        img_file = os.path.join(class_dir, img_path)
         img = cv2.imread(img_file)
-        if img is None :
-            print(f"Error : couldn't read image file{img_file}")
+        if img is None:
+            print(f"Error: couldn't read image file {img_file}")
             continue
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = hands.process(img_rgb)
-        if results.multi_hand_landmarks :
+        if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 for i in range(len(hand_landmarks.landmark)):
-                    x= hand_landmarks.landmark[i].x
+                    x = hand_landmarks.landmark[i].x
                     y = hand_landmarks.landmark[i].y
                     x_.append(x)
                     y_.append(y)
                 for i in range(len(hand_landmarks.landmark)):
                     x = hand_landmarks.landmark[i].x
                     y = hand_landmarks.landmark[i].y
-                    data_aux.append(x-min(x_))
-                    data_aux.append(y-min(y_))
-            data.append(data_aux)
-            labels.append(int(dir_))
-with open('data.pickle','wb') as f :
-    pickle.dump({'data':data,'labels':labels},f)
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
+            if len(data_aux) == 42:  # Ensure the length of data_aux is always 42
+                data.append(data_aux)
+                labels.append(int(dir_))
 
-with open('data.pickle','rb') as f :
+with open('data.pickle', 'wb') as f:
+    pickle.dump({'data': data, 'labels': labels}, f)
+
+#to see the content of pickle file    
+'''
+with open('data.pickle', 'rb') as f:
     dataset = pickle.load(f)
-print(f"number of samples :{len(dataset['data'])}")
-print(f"Labels:{set(dataset['labels'])}")
+print(f"Number of samples: {len(dataset['data'])}")
+print(f"Labels: {set(dataset['labels'])}")
 print(f"First sample data: {dataset['data'][0]}")
 print(f"First sample label: {dataset['labels'][0]}")
+'''
