98bd990926c7dc44a36dd2544ca84c6af71f2b0d
Salim Belkhir <143688571+SalimBelkhir@users.noreply.github.com>
2024-06-13 15:41:02 +0100

    Update FeatureExtraction.py

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,14 +1,13 @@
 import os
-import pickle
-import mediapipe as mp
 import cv2
+import mediapipe as mp
+import pickle
+import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 
-# Initialize Mediapipe Hands and Face Mesh
+# Initialize Mediapipe Hands
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
-mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.2)
 
 DATA_DIR = './data'
 data = []
@@ -26,31 +25,26 @@ def process_image(class_dir, img_path):
 
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     hand_results = hands.process(img_rgb)
-    face_results = face_mesh.process(img_rgb)
 
     if hand_results.multi_hand_landmarks:
         for hand_landmarks in hand_results.multi_hand_landmarks:
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
+            for landmark in hand_landmarks.landmark:
+                x = landmark.x
+                y = landmark.y
                 x_.append(x)
                 y_.append(y)
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
-                data_aux.append(x - min(x_))
-                data_aux.append(y - min(y_))
 
-    if face_results.multi_face_landmarks:
-        for face_landmarks in face_results.multi_face_landmarks:
-            for landmark in face_landmarks.landmark:
-                data_aux.append(landmark.x)
-                data_aux.append(landmark.y)
+        min_x = min(x_)
+        min_y = min(y_)
 
-    # Ensure the length of data_aux meets the expected format: 42 hand + 468*2 face = 42 + 936 = 978
-    if len(data_aux) == 42 +936:
+        for hand_landmarks in hand_results.multi_hand_landmarks:
+            for landmark in hand_landmarks.landmark:
+                data_aux.append(landmark.x - min_x)
+                data_aux.append(landmark.y - min_y)
+
         label = int(os.path.basename(class_dir))
         return data_aux, label
+
     return None
 
 with ThreadPoolExecutor() as executor:
@@ -71,13 +65,3 @@ with ThreadPoolExecutor() as executor:
 
 with open('data.pickle', 'wb') as f:
     pickle.dump({'data': data, 'labels': labels}, f)
-
-# To see the content of pickle file
-'''
-with open('data.pickle', 'rb') as f:
-    dataset = pickle.load(f)
-print(f"Number of samples: {len(dataset['data'])}")
-print(f"Labels: {set(dataset['labels'])}")
-print(f"First sample data: {dataset['data'][0]}")
-print(f"First sample label: {dataset['labels'][0]}")
-'''
